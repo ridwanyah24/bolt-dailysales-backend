a77d684026b579f785162764f9b72caa0f0cbe85
ridDev <ridwangames24@gmail.com>
2026-08-28 20:15:01 +0100

    Deleted tsconfig.tsbuildinfo

--- dist/common/roles.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Role } from './domain.types';
-export declare const ROLES_KEY = "roles";
-export declare const Roles: (...roles: Role[]) => import("@nestjs/common").CustomDecorator<string>;
